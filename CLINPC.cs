0b314024fa1216a0f97bdb7650c84ce16b117e6c
Sparcky <Lloydplays@iCloud.com>
2024-09-08 21:07:40 -0300

    A.1.2

--- CLINPC.cs
+++ CLINPC.cs
@@ -3,5 +3,5 @@ public class CLINPC : CLIPlayer
     public static string? Sexualty { get; set; } = "none";
     //"Gay","Pan","Str","Ace"
     public static bool canjoinparty { get; set; } = true;
-    
+    public static string? type { get; set; } = "none";
 }
--- CLISpace.cs
+++ CLISpace.cs
@@ -2,7 +2,7 @@ public class CLISpace
 {
     
     public static List<string?> Galaxy { get; set; } = new List<string?>();
-    //public static string? Solar_System { get; set; } = "none";
-    //public static string? Planet { get; set; } = "none";
+    public static List<string?> Solar_System { get; set; } = new List<string?>();
+    public static List<string?> Planet { get; set; } = new List<string?>();
 
 }
