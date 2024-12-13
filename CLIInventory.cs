2454ef9907da6a2352e16b4a0473825f9fa28914
Sparcky <87338814+Sparcky2257@users.noreply.github.com>
2024-12-13 17:04:51 +0000

    A.4.8

--- CLIInventory.cs
+++ CLIInventory.cs
@@ -1,6 +1,9 @@
+using System.Collections;
+
 public class CLIInventory
 {
     public static List<string?> Inventoryplace { get; set; } = new List<string?>(); // "place"
+    public static List<string?> Inventorytemp { get; set; } = new List<string?>(); // "place:itemid:itemid:itemid..."
     public static List<string?> Inventorystore { get; set; } = new List<string?>(); // "place:itemid"
     public static List<string?> items { get; set; } = new List<string?>(); // "itemid:name:description:data"
     public static void Inventory(string place = "null", string func1 = "null", string func2 = "null") //place ex player 
@@ -25,15 +28,31 @@ public class CLIInventory
                 }
                 else
                 {
-                    Inventoryfuncall(Inventoryplace[z]);
+                    Inventoryfuncall(Inventoryplace[z], func1, func2);
                 }
             }
-
         }
     }
     static void Inventoryfuncall(string place = "null", string func1 = "null", string func2 = "null")
     {
 
+        switch (func1)
+        {
+            case "null":
+            break;
+            case "Add":
+            break;
+            case "Remove":
+            break;
+            case "RM":
+            break;
+            case "Move":
+            break;
+            case "MV":
+            break;
+            default:
+            break;
+        }
     }
     public static void setup()
     {
